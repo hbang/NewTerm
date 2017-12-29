01b3ae671a241a3e6a176cde073fb867be27f1b7
Adam Demasi <adam@hbang.com.au>
2017-12-29 19:06:26 +1030

    [app] i just rewrote and expanded out some comments here nbd

--- NewTerm/HBNTTerminalController.m
+++ NewTerm/HBNTTerminalController.m
@@ -19,9 +19,9 @@
 }
 
 - (void)updateScreenSize {
-	// Send the terminal the actual size of our vt100 view. This should be
-	// called any time we change the size of the view. This should be a no-op if
-	// the size has not changed since the last time we called it.
+	// Send the terminal the actual size of our vt100 view. This should be called any time we change
+	// the size of the view. This should be a no-op if the size has not changed since the last time we
+	// called it.
 	[_pty setWidth:_viewController.screenWidth withHeight:_viewController.screenHeight];
 }
 
@@ -36,8 +36,7 @@
 	// The PTY will be sized correctly on the first call to layoutSubViews
 	_pty = [[HBNTPTY alloc] initWithFileHandle:_subProcess.fileHandle];
 
-	// Schedule an async read of the subprocess. Invokes our callback when
-	// data becomes available.
+	// Schedule an async read of the subprocess. Invokes our callback when data becomes available.
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dataAvailable:) name:NSFileHandleReadCompletionNotification object:_subProcess.fileHandle];
 	[_subProcess.fileHandle readInBackgroundAndNotify];
 }
@@ -46,25 +45,14 @@
 	NSData *data = notification.userInfo[NSFileHandleNotificationDataItem];
 
 	if (data.length == 0) {
-		// I would expect from the documentation that an EOF would be present as
-		// an entry in the userinfo dictionary as @"NSFileHandleError", but that is
-		// never present. Instead, it seems to just appear as an empty data
-		// message. This usually happens when someone just types "exit". Simply
-		// restart the subprocess when this happens.
-
-		// On EOF, either (a) the user typed "exit" or (b) the terminal never
-		// started in first place due to a misconfiguration of the BSD subsystem
-		// (can't find /bin/login, etc). To allow the user to proceed in case (a),
-		// display a message with instructions on how to restart the shell. We
-		// don't restart automatically in case of (b), which would put us in an
-		// infinite loop. Print a message on the screen with instructions on how
-		// to restart the process.
-
+		// zero-length data is an indicator of EOF. this can happen if the user exits the terminal by
+		// typing `exit`, or if there’s a catastrophic failure (e.g. /bin/login is broken). we print a
+		// message to the terminal saying the process ended, and then when the user presses any key, the
+		// tab will close
 		NSString *message = [NSString stringWithFormat:@"[%@]\r\n%@\r\n", NSLocalizedString(@"PROCESS_COMPLETED_TITLE", @"Title displayed when the terminal’s process has ended."), NSLocalizedString(@"PROCESS_COMPLETED_MESSAGE", @"Message indicating the user can press any key to close the tab.")];
-
 		[_viewController readInputStream:[message dataUsingEncoding:NSUTF8StringEncoding]];
-		_processEnded = YES;
 
+		_processEnded = YES;
 		[_subProcess stop];
 	} else {
 		// Forward the subprocess data into the terminal character handler
