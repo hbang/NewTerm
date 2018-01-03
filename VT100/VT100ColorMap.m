c19823ea9acfab2a7e6dc77558f50e75da04b846
Adam Demasi <adam@hbang.com.au>
2018-01-03 12:48:31 +1030

    [vt100] clean up color map a bit, add grayscale support

--- VT100/VT100ColorMap.m
+++ VT100/VT100ColorMap.m
@@ -24,22 +24,22 @@
 		_isDark = YES;
 		
 		// System 7.5 colors, why not?
-		_table[0]  = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:1.f]; // black
-		_table[1]  = [UIColor colorWithRed:0.6f green:0.0f blue:0.0f alpha:1.f]; // dark red
-		_table[2]  = [UIColor colorWithRed:0.0f green:0.6f blue:0.0f alpha:1.f]; // dark green
-		_table[3]  = [UIColor colorWithRed:0.6f green:0.4f blue:0.0f alpha:1.f]; // dark yellow
-		_table[4]  = [UIColor colorWithRed:0.0f green:0.0f blue:0.6f alpha:1.f]; // dark blue
-		_table[5]  = [UIColor colorWithRed:0.6f green:0.0f blue:0.6f alpha:1.f]; // dark magenta
-		_table[6]  = [UIColor colorWithRed:0.0f green:0.6f blue:0.6f alpha:1.f]; // dark cyan
-		_table[7]  = [UIColor colorWithRed:0.6f green:0.6f blue:0.6f alpha:1.f]; // dark white
-		_table[8]  = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:1.f]; // black
-		_table[9]  = [UIColor colorWithRed:1.0f green:0.0f blue:0.0f alpha:1.f]; // red
-		_table[10] = [UIColor colorWithRed:0.0f green:1.0f blue:0.0f alpha:1.f]; // green
-		_table[11] = [UIColor colorWithRed:1.0f green:1.0f blue:0.0f alpha:1.f]; // yellow
-		_table[12] = [UIColor colorWithRed:0.0f green:0.0f blue:1.0f alpha:1.f]; // blue
-		_table[13] = [UIColor colorWithRed:1.0f green:0.0f blue:1.0f alpha:1.f]; // magenta
-		_table[14] = [UIColor colorWithRed:0.0f green:1.0f blue:1.0f alpha:1.f]; // light cyan
-		_table[15] = [UIColor colorWithRed:1.0f green:1.0f blue:1.0f alpha:1.f]; // white
+		_table[kiTermScreenCharAnsiColorBlack]         = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorRed]           = [UIColor colorWithRed:0.6f green:0.0f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorGreen]         = [UIColor colorWithRed:0.0f green:0.6f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorYellow]        = [UIColor colorWithRed:0.6f green:0.4f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBlue]          = [UIColor colorWithRed:0.0f green:0.0f blue:0.6f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorMagenta]       = [UIColor colorWithRed:0.6f green:0.0f blue:0.6f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorCyan]          = [UIColor colorWithRed:0.0f green:0.6f blue:0.6f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorWhite]         = [UIColor colorWithRed:0.6f green:0.6f blue:0.6f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightBlack]   = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightRed]     = [UIColor colorWithRed:1.0f green:0.0f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightGreen]   = [UIColor colorWithRed:0.0f green:1.0f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightYellow]  = [UIColor colorWithRed:1.0f green:1.0f blue:0.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightBlue]    = [UIColor colorWithRed:0.0f green:0.0f blue:1.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightMagenta] = [UIColor colorWithRed:1.0f green:0.0f blue:1.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightCyan]    = [UIColor colorWithRed:0.0f green:1.0f blue:1.0f alpha:1.f];
+		_table[kiTermScreenCharAnsiColorBrightWhite]   = [UIColor colorWithRed:1.0f green:1.0f blue:1.0f alpha:1.f];
 	}
 	
 	return self;
@@ -83,8 +83,8 @@
 }
 
 - (UIColor *)colorAtIndex:(unsigned int)index {
-	// TODO(allen): The logic here is pretty ad hoc and could use some
-	// some helpful comments describing whats its doing.	It seems to work?	 
+	// TODO(allen): The logic here is pretty ad hoc and could use some some helpful comments
+	// describing whats its doing. It seems to work?
 	if (index == -1) {
 		return [UIColor clearColor];
 	} else if (index & COLOR_CODE_MASK) {
@@ -94,11 +94,11 @@
 			case CURSOR_BG:
 				return _backgroundCursor;
 			case BG_COLOR_CODE:
-				return _background;
+				return [UIColor clearColor];
 			default:
 				if (index & BOLD_MASK) {
 					if (index - BOLD_MASK == BG_COLOR_CODE) {
-						return _background;
+						return [UIColor clearColor];
 					} else {
 						return _foregroundBold;
 					}
@@ -109,16 +109,23 @@
 	} else {
 		index &= 0xff;
 		if (index < 16) {
+			// predefined color
 			return _table[index];
 		} else if (index < 232) {
+			// 256-color
 			index -= 16;
-			float components[] = {
-				(index / 36) ? ((index / 36) * 40 + 55) / 256.0 : 0,
-				(index % 36) / 6 ? (((index % 36) / 6) * 40 + 55 ) / 256.0 : 0,
-				(index % 6) ? ((index % 6) * 40 + 55) / 256.0 : 0,
+			CGFloat components[] = {
+				(index / 36) ? ((index / 36) * 40 + 55) / 255.0 : 0.0,
+				(index % 36) / 6 ? (((index % 36) / 6) * 40 + 55) / 255.0 : 0.0,
+				(index % 6) ? ((index % 6) * 40 + 55) / 255.0 : 0.0,
 				1.0
 			};
 			return [UIColor colorWithRed:components[0] green:components[1] blue:components[2] alpha:1.0f];
+		} else if (index < 256) {
+			// grayscale
+			index -= 232;
+			CGFloat gray = (index * 10 + 8) / 255.0;
+			return [UIColor colorWithWhite:gray alpha:1.0f];
 		} else {
 			return _foreground;
 		}
