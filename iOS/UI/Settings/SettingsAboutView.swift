ada41fc4bc6a614e8a624c14e89ceb2825531188
Adam Demasi <adam@hbang.com.au>
2021-04-07 16:33:14 +0930

    [ios] Implement an about screen

--- iOS/UI/Settings/SettingsAboutView.swift
+++ iOS/UI/Settings/SettingsAboutView.swift
@@ -21,14 +21,59 @@ struct SettingsAboutView: View {
 					.frame(height: 200)
 
 				Text("Version \(version)")
+					.fixedSize(horizontal: false, vertical: true)
+					.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
 					.multilineTextAlignment(.center)
 					.font(.system(size: 14, weight: .semibold))
 
-				Spacer(minLength: 15)
-
 				Text("This is a beta — thanks for trying it out! If you find any issues, please let us know.")
+					.fixedSize(horizontal: false, vertical: true)
+					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
 					.multilineTextAlignment(.center)
 					.font(.system(size: 12, weight: .regular))
+
+				NavigationLink(
+					destination: SafariViewControllerRepresentable(url: URL(string: "https://github.com/hbang/NewTerm/blob/master/LICENSE.md")!)
+						.navigationBarHidden(true),
+					label: { Text("Acknowledgements") }
+				)
+					.fixedSize(horizontal: false, vertical: true)
+					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
+
+				Text("If you like our work, please consider showing your appreciation with a small donation to the tip jar.")
+					.fixedSize(horizontal: false, vertical: true)
+					.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+					.multilineTextAlignment(.center)
+					.font(.system(size: 12, weight: .regular))
+
+				NavigationLink(
+					destination: SafariViewControllerRepresentable(url: URL(string: "https://hashbang.productions/donate/")!)
+						.navigationBarHidden(true),
+					label: { Text("Tip Jar") }
+				)
+					.fixedSize(horizontal: false, vertical: true)
+					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
+
+				Divider()
+					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
+					.fixedSize(horizontal: false, vertical: true)
+
+				NavigationLink(
+					destination: SafariViewControllerRepresentable(url: URL(string: "https://hashbang.productions/")!)
+						.navigationBarHidden(true),
+					label: {
+						VStack {
+							Image("hashbang")
+							Text("© HASHBANG Productions")
+								.foregroundColor(.secondary)
+								.padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+								.multilineTextAlignment(.center)
+								.font(.system(size: 12, weight: .regular))
+						}
+					}
+				)
+					.fixedSize(horizontal: false, vertical: true)
+					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
 			}
 		}
 		.navigationBarTitle("", displayMode: .inline)
