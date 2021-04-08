f477f58e1fb8325781eeea53102ed49e775ecab6
Adam Demasi <adam@hbang.com.au>
2021-04-08 11:49:38 +0930

    [ios] We miss you heaps, Dennis 🙁

--- iOS/UI/Settings/SettingsAboutView.swift
+++ iOS/UI/Settings/SettingsAboutView.swift
@@ -16,13 +16,13 @@ struct SettingsAboutView: View {
 
 	var body: some View {
 		ScrollView {
-			VStack {
+			VStack(spacing: 15) {
 				LogoHeaderViewRepresentable()
 					.frame(height: 200)
 
 				Text("Version \(version)")
 					.fixedSize(horizontal: false, vertical: true)
-					.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+					.padding(EdgeInsets(top: 15, leading: 15, bottom: 0, trailing: 15))
 					.multilineTextAlignment(.center)
 					.font(.system(size: 14, weight: .semibold))
 
@@ -35,7 +35,10 @@ struct SettingsAboutView: View {
 				NavigationLink(
 					destination: SafariViewControllerRepresentable(url: URL(string: "https://github.com/hbang/NewTerm/blob/master/LICENSE.md")!)
 						.navigationBarHidden(true),
-					label: { Text("Acknowledgements") }
+					label: {
+						Text("Acknowledgements")
+							.frame(width: 300)
+					}
 				)
 					.fixedSize(horizontal: false, vertical: true)
 					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
@@ -49,7 +52,10 @@ struct SettingsAboutView: View {
 				NavigationLink(
 					destination: SafariViewControllerRepresentable(url: URL(string: "https://hashbang.productions/donate/")!)
 						.navigationBarHidden(true),
-					label: { Text("Tip Jar") }
+					label: {
+						Text("Tip Jar")
+							.frame(width: 300)
+					}
 				)
 					.fixedSize(horizontal: false, vertical: true)
 					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
@@ -74,6 +80,14 @@ struct SettingsAboutView: View {
 				)
 					.fixedSize(horizontal: false, vertical: true)
 					.padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
+
+				Text("Dedicated to Dennis Bednarz (2000 — 2019), a friend and visionary of the iOS community taken from us too soon.")
+					.fixedSize(horizontal: false, vertical: true)
+					.frame(width: 260)
+					.foregroundColor(.secondary)
+					.padding(EdgeInsets(top: 15, leading: 15, bottom: 30, trailing: 15))
+					.multilineTextAlignment(.center)
+					.font(.system(size: 12, weight: .regular))
 			}
 		}
 		.navigationBarTitle("", displayMode: .inline)
